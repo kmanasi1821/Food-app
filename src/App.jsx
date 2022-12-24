4479d463cd8d1e223ab7aeb2d747182994dffe38
Manasi Wagh <94756252+kmanasi1821@users.noreply.github.com>
2022-12-24 12:12:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Cart from "./pages/Cart";
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/Food-app">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
